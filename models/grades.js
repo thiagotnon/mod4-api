ed40b548c8c8b7e4e6148ef9de6348cc64691fb9
yakobus <thiagotnon@gmail.com>
2020-09-04 00:47:46 -0300

    api

---
+++ models/grades.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const gradeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  subject: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  value: {
+    type: Number,
+    require: true
+  },
+  lastModified: {
+    type: Date,
+    require: true
+  },
+});
+
+export default mongoose.model('Grades', gradeSchema);
